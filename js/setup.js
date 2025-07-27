e27ff610de7f4839e6d2d04fe7066aae0d6588cc
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-07-27 15:18:45 +0200

    test

---
+++ js/setup.js
@@ -0,0 +1,6 @@
+import { PichaiUX } from 'https://dreamforge-forging-our-dreams-in-tech.github.io/Pichai-UX/imports.js';
+
+let options = {}
+
+let pichai = new PichaiUX(options);
+pichai.initialize();
